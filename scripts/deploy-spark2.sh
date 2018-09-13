85e68a26db8faf4035d1b4da0319ef63bda3c27f
zhichao-li <zhichao.li@intel.com>
2018-09-13 13:11:00 +0800

    scala2.10

--- scripts/deploy-spark2.sh
+++ scripts/deploy-spark2.sh
@@ -60,15 +60,14 @@ mv spark-version/2.0/pom.xml spark-version/2.0/pom.xml.origin
 cat spark-version/2.0/pom.xml.origin | sed 's/<artifactId>2.0<\/artifactId>/<artifactId>2.0-${SPARK_PLATFORM}<\/artifactId>/' > spark-version/2.0/pom.xml
 
 function deploy {
-    mvn clean install -DskipTests -P sign -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3
-    cd spark-version && mvn deploy -DskipTests -P sign -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3 && cd ..
-    cd dl && mvn deploy -DskipTests -P sign -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3 && cd ..
-    cd dist && mvn deploy -DskipTests -P sign -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3 && cd ..
+    mvn clean install -DskipTests  -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3
+    cd spark-version && mvn install -DskipTests  -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3 && cd ..
+    cd dl && mvn install -DskipTests  -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3 && cd ..
+    cd dist && mvn install -DskipTests -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3 && cd ..
 }
 
-deploy 2.1.1 SPARK_2.1 '-P spark_2.x'
-deploy 2.2.0 SPARK_2.2 '-P spark_2.x'
-deploy 2.3.1 SPARK_2.3 '-P spark_2.x'
+deploy 2.1.1 SPARK_2.1 '-P spark_2.x -Dscala.version=2.10.6 -Dscala.major.version=2.10'
+
 
 mv dl/pom.xml.origin dl/pom.xml
 mv spark-version/1.5-plus/pom.xml.origin spark-version/1.5-plus/pom.xml
